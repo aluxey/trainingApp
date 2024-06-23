bc18c22004d4bf1e5904feade64631089f0fe787
Aurelien <aurelien.luxeyy@gmail.com>
2024-06-23 14:07:29 +0200

    build added

--- src/pages/Anime.jsx
+++ src/pages/Anime.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { db } from '../firebase/firebase';
 import { collection, addDoc, getDocs, query, where, deleteDoc, doc } from 'firebase/firestore';
 import { useAuth } from '../firebase/AuthContext';
