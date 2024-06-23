0f3d06fe2d7ff6ec5cc8a85f4dab33280e316c6b
Aurelien <aurelien.luxeyy@gmail.com>
2024-06-23 14:11:43 +0200

    default anime page

--- src/pages/Anime.jsx
+++ src/pages/Anime.jsx
@@ -1,17 +1,6 @@
-import React, {useState} from 'react'
-import { db } from '../firebase/firebase';
-import { collection, addDoc, getDocs, query, where, deleteDoc, doc } from 'firebase/firestore';
-import { useAuth } from '../firebase/AuthContext';
-import '../style/widget.css';
+import React from 'react'
 
 const Anime = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [anime, setMovie] = useState(null);
-    const [error, setError] = useState(null);
-    const [watchLaterA, setWatchLaterA] = useState([]);
-    const { currentUser } = useAuth();
-    const apiKey = process.env.REACT_APP_OMDB_API_KEY;
-    const [loading, setLoading] = useState(false);
 
   return (
     <div>
